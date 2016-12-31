b70f62bc17ef9a454179cf8820a6d02fdd334e31
Lucas Ghibert <lghibert@emi.u-bordeaux1.fr>
2016-12-31 15:38:01 +0100

    elimination superflu

--- client.py
+++ client.py
@@ -5,11 +5,11 @@ import socket
 s_client = socket.socket(socket.AF_INET6,socket.SOCK_STREAM,proto=0)
 s_client.connect(('',12345))
 grids = [grid(), grid()]
-joueur=""
-rcvspect=""
+joueur="" # Nom joueur / machine
+rcvspect="" # Nom spectateur ? N° ?
 i=0
-s1=s2=null=0
-replay=1
+s1=s2=null=0 # Score
+replay=1 # Booleen pour rejouer
 joueur=s_client.recv(1024).decode()
 if(joueur=="spectateur"):
 	print(joueur)
@@ -26,9 +26,8 @@ while True :
 				shot = -1
 				if(joueur!="spectateur"):
 					while shot <0 or shot >=NB_CELLS:
-						while shot <0 or shot >=NB_CELLS:
-							shot = int(input ("quelle case allez-vous jouer ?"))
-							#print("cells",grids[0].cells[shot])
+						shot = int(input ("quelle case allez-vous jouer ?"))
+						#print("cells",grids[0].cells[shot])
 						if (grids[0].cells[shot] != EMPTY):
 							print("Case occupée %d" %shot)
 							grids[1].cells[shot] = grids[0].cells[shot]
@@ -39,66 +38,57 @@ while True :
 							grids[1].play(1, shot)
 							grids[1].display()
 					s_client.send(str(shot).encode())
-					if grids[0].gameOver() != -1:
+					if grids[0].gameOver() != -1: # Pourquoi en deux fois ?
 						print("game over")
 						grids[0].display()
 						if grids[0].gameOver() == 1:
 							print("You win !")
-							break
 						elif grids[0].gameOver() == 2:
 							print("You lose !")
-							break	
 						else:
 							print("match nul !")
-							break
+					
 					joueur=s_client.recv(1024).decode()		
 					grids[0].play(2,int(joueur))
 					#grids[1].play(2,int(joueur))
 					#grids[1].display()
-					if grids[0].gameOver() != -1:
+					if grids[0].gameOver() != -1: # Pourquoi en deux fois ?
 						print("game over")
 						grids[0].display()
 						if grids[0].gameOver() == 1:
 							print("You win !")
-							break
 						elif grids[0].gameOver() == 2:
 							print("You lose !")
-							break		
 						else:
 							print("match nul !")
-							break	
+					
 				else:
 					#grids[0].display()
 					
 					rcvspect=s_client.recv(1024).decode()
 					r=int(rcvspect)
-					if(r>=0 and r<9):
+					if(r>=0 and r<9): # i ?
 						if(i%2==0):
-							i+=1
 							grids[0].play(1,int(rcvspect))
 							grids[0].display()
 						else:
 							grids[0].play(2,int(rcvspect))
 							grids[0].display()
-							i+=1	
+						i+=1
 					if grids[0].gameOver() != -1:
 						print("game over")
 						grids[0].display()
 						if grids[0].gameOver() == 1:
 							s1+=1
 							print("Joueur 1 win !")
-							break
 						elif grids[0].gameOver() == 2:
 							s2+=1
 							print("Joueur 2 win!")
 							null+=1
-							break	
 						else:
-							print("match nul !")
-							break										
+							print("match nul !")									
 		else:	
 			print("Adversaire deconnecte,vous avez gagne")
-			break
 		
 		print("Partie termine")
 		replay=int(input ("voulez vous rejouer la partie ? Taper 1 pour 'Oui' ou autre chiffre pour 'Non' "))
