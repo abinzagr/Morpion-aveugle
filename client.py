bc40da89a471413356e03552a95a17b3575bd87c
Lucas Ghibert <lghibert@emi.u-bordeaux1.fr>
2017-01-04 16:39:45 +0100

    correction erreur, application convention codage (langue, mise en page), factorisation du code, suppression redondances, amélioration affichage

--- client.py
+++ client.py
@@ -1,106 +1,85 @@
-#client.py
-
 from grid import *
 import socket
+
+def displayWinner(grid):
+	if grid.gameOver() != -1:
+		print("Partie terminée.")
+		grid.display()
+		if grid.gameOver() == 1:
+			print("Joueur 1 gagne !")
+		elif grid.gameOver() == 2:
+			print("Joueur 2 gagne !")
+		else:
+			print("Match nul !")
+
 s_client = socket.socket(socket.AF_INET6,socket.SOCK_STREAM,proto=0)
 s_client.connect(('',12345))
-grids = [grid(), grid()]
-joueur="" # Nom joueur / machine
-rcvspect="" # Nom spectateur ? N° ?
-i=0
-s1=s2=null=0 # Score
-replay=1 # Booleen pour rejouer
-joueur=s_client.recv(1024).decode()
-if(joueur=="spectateur"):
-	print(joueur)
-if joueur=="premier joueur":
-	print("En attente d'un Adversaire...")
-	data=s_client.recv(1024)
-	grids[0].play(2,int(data))
-if joueur!="spectateur":	
-	grids[1].display()
-while True :		
-	if(replay==1):
-		if(joueur!="Adversaire Deconnecté"):
-			while grids[0].gameOver() == -1:
-				shot = -1
-				if(joueur!="spectateur"):
-					while shot <0 or shot >=NB_CELLS:
-						shot = int(input ("quelle case allez-vous jouer ?"))
-						#print("cells",grids[0].cells[shot])
-						if (grids[0].cells[shot] != EMPTY):
-							print("Case occupée %d" %shot)
-							grids[1].cells[shot] = grids[0].cells[shot]
-							grids[1].display()
-							shot=-1
-						else:
-							grids[0].play(1,shot)
-							grids[1].play(1, shot)
-							grids[1].display()
-					s_client.send(str(shot).encode())
-					if grids[0].gameOver() != -1: # Pourquoi en deux fois ?
-						print("game over")
-						grids[0].display()
-						if grids[0].gameOver() == 1:
-							print("You win !")
-						elif grids[0].gameOver() == 2:
-							print("You lose !")
-						else:
-							print("match nul !")
-					
-					joueur=s_client.recv(1024).decode()		
-					grids[0].play(2,int(joueur))
-					#grids[1].play(2,int(joueur))
-					#grids[1].display()
-					if grids[0].gameOver() != -1: # Pourquoi en deux fois ?
-						print("game over")
-						grids[0].display()
-						if grids[0].gameOver() == 1:
-							print("You win !")
-						elif grids[0].gameOver() == 2:
-							print("You lose !")
-						else:
-							print("match nul !")
-					
-				else:
-					#grids[0].display()
-					
-					rcvspect=s_client.recv(1024).decode()
-					r=int(rcvspect)
-					if(r>=0 and r<9): # i ?
-						if(i%2==0):
-							grids[0].play(1,int(rcvspect))
-							grids[0].display()
-						else:
-							grids[0].play(2,int(rcvspect))
-							grids[0].display()
-						i+=1
-					if grids[0].gameOver() != -1:
-						print("game over")
-						grids[0].display()
-						if grids[0].gameOver() == 1:
-							s1+=1
-							print("Joueur 1 win !")
-						elif grids[0].gameOver() == 2:
-							s2+=1
-							print("Joueur 2 win!")
-							null+=1
-						else:
-							print("match nul !")									
-		else:	
-			print("Adversaire deconnecte,vous avez gagne")
-		
-		print("Partie termine")
-		replay=int(input ("voulez vous rejouer la partie ? Taper 1 pour 'Oui' ou autre chiffre pour 'Non' "))
-		print(replay)
-		if(replay!=1):
-			break
-if(replay==1):
-	if joueur=="premier joueur":
-		#print("En attente d'un Adversaire...")
-		data=s_client.recv(1024)
-		grids[0].play(2,int(data))
 
-					
-	
+grids = [grid(), grid()]
+joueur = "" # Type de joueur
+rcvspect = "" # Coup joué reçu par spectateur
+s1 = s2 = null = 0 # Score
+replay = 1 # Booleen pour rejouer
+
+while replay == 1:
+	joueur = s_client.recv(1024).decode()
+	if joueur == "spectateur":
+		print("Vous êtes spectateur.")
+	if joueur == "premier joueur":
+		print("Vous êtes le Joueur 1.")
+		print("En attente d'un Adversaire...")
+		adv_recv = s_client.recv(1024).decode()
+	if joueur == "second joueur":
+		print("Vous êtes le Joueur 2.")
+		first_shot = s_client.recv(1024).decode()
+		grids[0].play(1,int(first_shot))
+
+	if joueur != "Adversaire déconnecté":
+		i = 0
+		while grids[0].gameOver() == -1:
+			if joueur != "spectateur":
+				grids[1].display()
+				shot = -1
+				if joueur == "premier joueur": # Numéro du joueur actif
+					j = 1
+				else:
+					j = 2
+				# Joueur actif
+				while shot < 0 or shot >= NB_CELLS:
+					shot = int(input ("Quelle case allez-vous jouer ? "))
+					if grids[0].cells[shot] != EMPTY:
+						print("Case %d occupée" %shot)
+						grids[1].cells[shot] = grids[0].cells[shot]
+						shot = -1
+					else:
+						grids[0].play(j, shot)
+						grids[1].play(j, shot)
+					grids[1].display()
+				s_client.send(str(shot).encode())
+				displayWinner(grids[0])
+			
+				# Joueur passif
+				if j == 1: # Numéro du joueur passif
+					j = 2
+				else:
+					j = 1
+				shot = s_client.recv(1024).decode()		
+				grids[0].play(j,int(shot))
+				displayWinner(grids[0])			
+			else:   # Spectateur
+				rcvspect = s_client.recv(1024).decode()
+				r = int(rcvspect)
+				if r >= 0 and r < NB_CELLS:
+					if i%2 == 0:
+						grids[0].play(1,r)
+					else:
+						grids[0].play(2,r)
+					grids[0].display()
+					i += 1
+				displayWinner(grids[0])									
+	else:	
+		print("Adversaire déconnecté.")
+		
+	replay=int(input ("Voulez vous rejouer une partie ? Taper 1 pour 'Oui' ou un autre chiffre pour 'Non'. "))
+
 s_client.close()
--- serveur.py
+++ serveur.py
@@ -1,8 +1,8 @@
-#serveur.py
-
 import socket,select,threading,sys
 from grid import *
 
+# Pour sauvegarde etat grille ?
+"""
 #notif connexion d'un client aux autres clients	
 def notif_connet_joueur(sock,message) :
 	if socket != server_socket and socket != sock:
@@ -14,44 +14,43 @@ def notif_connet_joueur(sock,message) :
                 list_joueur.remove(sock)
 
 #envoie grille à un joueur
-
+"""
 
 
 list_joueur = []
-#list_observateur =[]
 server_socket = socket.socket(socket.AF_INET6,socket.SOCK_STREAM,proto=0)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind (('', 12345)) 
 server_socket.listen(1)
 list_joueur.append(server_socket)
 
-while 1:
-	#verification si un joueur souhaite se connecter + ajout à la liste 
+while True:
+	# Attente connexion d'un joueur + ajout à la liste 
 	read_sockets, write_sockets, error_sockets = select.select(list_joueur, [], [])
 	for sock in read_sockets:
-		if sock == server_socket:
+		if sock == server_socket: # Attribution joueur/spectateur
 			sockfd, addr = server_socket.accept()
 			list_joueur.append(sockfd)
-			print("Client (" + str(addr[1]) + ") connected")
-			if(len(list_joueur)==2):
+			print("Client (" + str(addr[1]) + ") connecté.")
+			if len(list_joueur) == 2:
 				sockfd.send('premier joueur'.encode())
-			elif(len(list_joueur)==3):
+			elif len(list_joueur) == 3:
 				sockfd.send('second joueur'.encode())
+				list_joueur[1].send('adversaire trouve'.encode())
 			else:
 				sockfd.send('spectateur'.encode())
 		else:
 			try:
 				mesg = sock.recv(1024).decode()
 			except ConnectionResetError:
-				print("connexion perdue")
-			if len(mesg) == 0:	
+				print("Connexion perdue.")
+			if len(mesg) == 0:
 				list_joueur.remove(sock)
-				if range(len(list_joueur))==2:
-					list_joueur[1].send('Adversaire Deconnecté'.encode())
+				if range(len(list_joueur)) == 2:
+					list_joueur[1].send('Adversaire déconnecté'.encode())
 				sock.close()
-			else:
-				for i in range(len(list_joueur)):
-                                        #Le message est envoyer à tous les joueurs et pas à celui qui l'a envoyé
+			else: # Envoi coup joué
+				for i in range(len(list_joueur)): # Pourrait éviter d'envoyer au serveur ?
                                         if list_joueur[i] != server_socket and list_joueur[i] != sock:
                                                 list_joueur[i].send(mesg.encode())
 						
